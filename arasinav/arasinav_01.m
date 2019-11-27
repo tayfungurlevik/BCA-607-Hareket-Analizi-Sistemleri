bcbdc4633a4aefd5ff4329c35e3d7f33ad11c645
Tayfun Gürlevik <tgurlevik@gmail.com>
2019-11-27 16:18:25 +0300

    diz ve bel swap islemi

--- arasinav/arasinav_01.m
+++ arasinav/arasinav_01.m
@@ -1,7 +1,8 @@
 
 dosya_on_ad='fettah_sut2_C001H001S0001';
 dosya_uzanti='.jpg';
-for n=1:105
+n=85
+%for n=1:105
 dosya_adi=strcat(dosya_on_ad,sprintf( '%06d', n ) ,dosya_uzanti);
 RGB=imread(dosya_adi);
 I=rgb2gray(RGB);
@@ -13,30 +14,33 @@ I2=I-background;
 I3=imadjust(I2,[0.3 0.7],[]);
 [T EM]=graythresh(I3);
 BW=imbinarize(I3,T);
-BW=medfilt2(BW,[3 3]);
-BW = bwareaopen(BW,15);
 %maskeleme islemi icin Y si 135 den kucuk olan tum pixelleri 0 yaptim.
 BW(:,1:135)=0;
+BW=medfilt2(BW,[3 3]);
+BW = bwareaopen(BW,15);
+
+
 imshow(BW);
 hold on
 [B,L]=bwboundaries(BW,'noholes');
 stats=regionprops(L,I,'Area','WeightedCentroid','Centroid','Perimeter');
-
-renk='ygbmcr';
+%bel ve diz karistigi icin 60-85 frame arasi
+[stats(4),stats(5)]=deal(stats(5),stats(4));
+renk='ygbmc';
 for i=1:length(stats)
-   centroid=stats(i).Centroid;
+   centroid=stats(i).WeightedCentroid;
    
    plot(centroid(1),centroid(2),'O','Color',renk(i));
    text(centroid(1)+5,centroid(2),sprintf('%3.3f,%3.3f',centroid(1),centroid(2)),'Color',renk(i),'FontSize',12);
    
 end
 for i=1:length(stats)-1
-    centroid=stats(i).Centroid;
-    centroid2=stats(i+1).Centroid;
+    centroid=stats(i).WeightedCentroid;
+    centroid2=stats(i+1).WeightedCentroid;
     line([centroid(1),centroid2(1)],[centroid(2),centroid2(2)],'Color','white','LineStyle','--');
 end
 hold off
-pause 
-end
+%pause 
+%end
 
 
