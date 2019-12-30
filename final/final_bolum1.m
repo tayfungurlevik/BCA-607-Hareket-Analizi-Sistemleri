bc23abd888bb1d8f6465a3f33d8efffac5672108
Tayfun Gürlevik <tgurlevik@gmail.com>
2019-12-30 17:02:19 +0300

    1. kısım bölüm 2 yapıldı

--- final/final_bolum1.m
+++ final/final_bolum1.m
@@ -1,7 +1,50 @@
 %% vicon dosyasindan labellarin pozisyonlarini okuma islemi
-filepath='hafta13\vicon_all_labelled.txt';
-A = [30,0,0];
-B = [0,40,0];
-C = [0,0,10];
+filepath='vicon_parsed.txt';
+load 'vicon_parsed.txt';
+frames=vicon_parsed(:,1);
+M1=vicon_parsed(:,2:4);
+M2=vicon_parsed(:,5:7);
+M3=vicon_parsed(:,8:10);
+M4=vicon_parsed(:,11:13);
+M5=vicon_parsed(:,14:16);
+video=true;
+if video
+   aviObj=VideoWriter('final.mp4','MPEG-4');
+   aviObj.FrameRate=25;
+   open(aviObj);
+end
 
-fill3(A,B,C,'red')
+for i=1:length(frames)
+    plot3(M1(:,1),M1(:,3),M1(:,2),'--r')
+    grid on
+    axis tight
+    hold on
+    plot3(M2(:,1),M2(:,3),M2(:,2),'--g')
+    plot3(M3(:,1),M3(:,3),M3(:,2),'--b')
+    plot3(M4(:,1),M4(:,3),M4(:,2),'--y')
+    plot3(M5(:,1),M5(:,3),M5(:,2),'--c')
+
+    
+
+    X(1,1)=M1(i,1);
+    X(1,2)=M3(i,1);
+    X(1,3)=M5(i,1);
+    Y(1,1)=M1(i,3);
+    Y(1,2)=M3(i,3);
+    Y(1,3)=M5(i,3);
+    Z(1,1)=M1(i,2);
+    Z(1,2)=M3(i,2);
+    Z(1,3)=M5(i,2);
+
+
+    fill3(X,Y,Z,'red');
+    if video
+       frame=getframe(gcf);
+       writeVideo(aviObj,frame);
+    end
+    hold off
+end
+if video
+   close(aviObj); 
+end
+
