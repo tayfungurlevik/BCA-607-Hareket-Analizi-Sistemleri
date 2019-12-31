7fbe3b3fb7b740a9346ac023cbcaff4db36e5f9c
Tayfun Gürlevik <tgurlevik@gmail.com>
2019-12-31 15:51:44 +0300

    bolum 1 roll pitch yaw çizildi

--- final/final_bolum1.m
+++ final/final_bolum1.m
@@ -1,5 +1,5 @@
 %% vicon dosyasindan labellarin pozisyonlarini okuma islemi
-filepath='vicon_parsed.txt';
+
 load 'vicon_parsed.txt';
 frames=vicon_parsed(:,1);
 M1=vicon_parsed(:,2:4);
@@ -17,7 +17,7 @@ end
 for i=1:length(frames)
     plot3(M1(:,1),M1(:,3),M1(:,2),'--r')
     grid on
-    axis tight
+    axis equal
     hold on
     plot3(M2(:,1),M2(:,3),M2(:,2),'--g')
     plot3(M3(:,1),M3(:,3),M3(:,2),'--b')
@@ -47,4 +47,29 @@ end
 if video
    close(aviObj); 
 end
+%% Roll,pitch ve yaw degerlerinin hesaplanmasi
+roll=zeros(1,length(frames));
+pitch=zeros(1,length(frames));
+yaw=zeros(1,length(frames));
+for n=1:length(frames)
+    P1=M1(n,:);
+    P2=M3(n,:);
+    P3=M5(n,:);
+    [P1(1,2),P1(1,3)]=deal(P1(1,3),P1(1,2));
+    [P2(1,2),P2(1,3)]=deal(P2(1,3),P2(1,2));
+    [P3(1,2),P3(1,3)]=deal(P3(1,3),P3(1,2));
+    v1=P2-P1;
+    v2=cross(P3-P1,v1);
+    i=v1/norm(v1);
+    j=v2/norm(v2);
+    k=cross(i,j);
+    R=[i(1),j(1),k(1);i(2),j(2),k(2);i(3),j(3),k(3)];
 
+    eul=rotm2eul(R,'XYZ');
+    roll(n)=eul(1);
+    pitch(n)=eul(2);
+    yaw(n)=eul(3);
+     
+end
+
+plot(1:length(frames),radtodeg(roll));
